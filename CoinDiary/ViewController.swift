d2c29ac4e67f19c818b7ff3a84f5cd532309e45f
taehyeong.um <taehyeong.um@buxikorea.com>
2022-09-15 14:31:00 +0900

    init

---
+++ CoinDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinDiary
+//
+//  Created by 엄태형 on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
